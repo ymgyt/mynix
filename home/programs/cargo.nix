2810163c4e4d6e54fde4c5218e12809bce01302d
ymgyt <yamaguchi7073xtt@gmail.com>
2023-06-05 00:52:35 +0900

    feat: update cargo subcommands

--- home/programs/cargo.nix
+++ home/programs/cargo.nix
@@ -1,5 +1,13 @@
 { pkgs, ...}: {
 	home.packages = with pkgs; [
+		cargo-criterion
+		cargo-deps
+		cargo-expand
+		cargo-insta
 		cargo-make
+		cargo-nextest
+		cargo-udeps
+		cargo-sort
+		cargo-vet
 	];
 }
