621da4dc600910a3b1cf07a89f1b2fc900e7bbee
ymgyt <yamaguchi7073xtt@gmail.com>
2023-06-09 11:24:33 +0900

    feat: add community cache

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
     auto-optimise-store = true;
 
     eval-cache = true;
+
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      # https://app.cachix.org/cache/nix-community#pull
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   inputs = {
