e9b75f13d7eea405b3e3ef4a600065accaab2d47
ymgyt <yamaguchi7073xtt@gmail.com>
2023-06-09 11:53:22 +0900

    style: apply format

--- home/base/programs/default.nix
+++ home/base/programs/default.nix
@@ -16,5 +16,8 @@
 
     # Says
     ponysay
+
+    # nix lang server
+    nil
   ];
 }
--- home/base/shell/zsh/default.nix
+++ home/base/shell/zsh/default.nix
@@ -2,7 +2,7 @@
 
   programs.zsh = {
     enable = true;
-  };  
+  };
 
   home.file.".zshrc".source = ./.zshrc;
   home.file.".zshenv".source = ./.zshenv;
