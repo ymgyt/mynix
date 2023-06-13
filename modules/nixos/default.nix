8e83549425a8444673707ce94d6cc6b1fababd38
ymgyt <yamaguchi7073xtt@gmail.com>
2023-06-13 19:08:48 +0900

    refactor: remove duplicate packages

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -87,11 +87,8 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    bat
     fd
     git
-    helix
-    rustup
   ];
   environment.extraInit = ''
   # Disable ssh-askpass
