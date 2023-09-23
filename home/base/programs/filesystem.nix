211e54a00c9e2bc74fcf303036db2b8168ab4a80
ymgyt <yamaguchi7073xtt@gmail.com>
2023-09-23 14:46:02 +0900

    fix: disable exa

--- home/base/programs/filesystem.nix
+++ home/base/programs/filesystem.nix
@@ -1,9 +1,11 @@
 { pkgs, ... }: {
-  programs.exa = {
-    enable = true;
-    enableAliases = true;
-    icons = true;
-  };
+  # exa removed
+  # and eza not published yet in home-manager
+  # programs.exa = {
+  #   enable = true;
+  #   enableAliases = true;
+  #   icons = true;
+  # };
 
   # Not published yet in 23.05-darwin
   # home.packages = with pkgs; [
