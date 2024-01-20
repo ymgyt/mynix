801c8059b16ad99f88e8f68d1b5c5a9909631164
ymgyt <yamaguchi7073xtt@gmail.com>
2024-01-20 20:16:34 +0900

    feat: add helix languages.toml

--- home/base/editor/helix/default.nix
+++ home/base/editor/helix/default.nix
@@ -1,7 +1,8 @@
 { ... }: {
   programs.helix = {
-    # TODO: currently using master or specific branch
+    # Currently use my fork
     enable = false;
   };
   xdg.configFile."helix/config.toml".source = ./config.toml;
+  xdg.configFile."helix/languages.toml".source = ./languages.toml;
 }
