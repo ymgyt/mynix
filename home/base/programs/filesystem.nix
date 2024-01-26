88ef5af5be4d4a8904d26580fe90e4d8f1d9e721
ymgyt <yamaguchi7073xtt@gmail.com>
2024-01-26 21:01:35 +0900

    feat: enable eza

--- home/base/programs/filesystem.nix
+++ home/base/programs/filesystem.nix
@@ -1,14 +1,1 @@
-{ pkgs, ... }: {
-  # exa removed
-  # and eza not published yet in home-manager
-  # programs.exa = {
-  #   enable = true;
-  #   enableAliases = true;
-  #   icons = true;
-  # };
-
-  # Not published yet in 23.05-darwin
-  # home.packages = with pkgs; [
-  #   yazi
-  # ];
-}
+{ ... }: { programs.eza = { enabme = true; }; }
