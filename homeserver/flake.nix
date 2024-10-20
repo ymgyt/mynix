9845ebb42ae8a1750418c5b59c1be6ca413ad5eb
ymgyt <yamaguchi7073xtt@gmail.com>
2024-10-21 01:32:54 +0900

    refactor(homeserver): remove duplicate node configs

--- homeserver/flake.nix
+++ homeserver/flake.nix
@@ -18,8 +18,7 @@
       inputs.flake-utils.follows = "flake-utils";
     };
     mysecrets = {
-      url =
-        "github:ymgyt/mynix.secrets/7740a5f2efd0f8bec2f706e3153b89b4a1492f80";
+      url = "github:ymgyt/mynix.secrets/7740a5f2efd0f8bec2f706e3153b89b4a1492f80";
       flake = false;
     };
 
@@ -30,8 +29,7 @@
     };
 
     opentelemetry-cli = {
-      url =
-        "github:ymgyt/opentelemetry-cli/ffba78aa5d2f1271a29d9e911028af8a28b1c589";
+      url = "github:ymgyt/opentelemetry-cli/ffba78aa5d2f1271a29d9e911028af8a28b1c589";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
@@ -49,96 +47,105 @@
     };
   };
 
-  outputs = { self, nixpkgs, deploy-rs, flake-utils, telemetryd, ragenix
-    , mysecrets, opentelemetry-cli, kvsd, syndicationd }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      deploy-rs,
+      flake-utils,
+      telemetryd,
+      ragenix,
+      mysecrets,
+      opentelemetry-cli,
+      kvsd,
+      syndicationd,
+    }:
     let
       spec = {
         user = "ymgyt";
         defaultGateway = "192.168.10.1";
         nameservers = [ "8.8.8.8" ];
-        inherit telemetryd ragenix mysecrets opentelemetry-cli kvsd
-          syndicationd;
+        inherit
+          telemetryd
+          ragenix
+          mysecrets
+          opentelemetry-cli
+          kvsd
+          syndicationd
+          ;
       };
-    in {
-      nixosConfigurations = {
-        rpi4-01 = nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux";
-          specialArgs = spec;
-          modules = [ ./hosts/rpi4-01.nix ];
+    in
+    {
+      nixosConfigurations =
+        let
+          mkHost = (
+            module:
+            nixpkgs.lib.nixosSystem {
+              system = "aarch64-linux";
+              specialArgs = spec;
+              modules = [ module ];
+            }
+          );
+        in
+        {
+          rpi4-01 = mkHost ./hosts/rpi4-01.nix;
+          rpi4-02 = mkHost ./hosts/rpi4-02.nix;
+          rpi4-03 = mkHost ./hosts/rpi4-03.nix;
+          rpi4-04 = mkHost ./hosts/rpi4-04.nix;
         };
-        rpi4-02 = nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux";
-          specialArgs = spec;
-          modules = [ ./hosts/rpi4-02.nix ];
-        };
-        rpi4-03 = nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux";
-          specialArgs = spec;
-          modules = [ ./hosts/rpi4-03.nix ];
-        };
-        rpi4-04 = nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux";
-          specialArgs = spec;
-          modules = [ ./hosts/rpi4-04.nix ];
-        };
-      };
 
       deploy = {
         # Deployment options applied to all nodes
         sshUser = spec.user;
         # User to which profile will be deployed.
         user = "root";
-        sshOpts = [ "-p" "22" "-F" "./etc/ssh.config" ];
+        sshOpts = [
+          "-p"
+          "22"
+          "-F"
+          "./etc/ssh.config"
+        ];
 
         fastConnection = false;
         autoRollback = true;
         magicRollback = true;
 
         # Or setup cross compilation
-        remoteBuild = true;
+        remoteBuild = false;
 
-        nodes = {
-          rpi4-01 = {
-            hostname = "rpi4-01";
-            profiles.system = {
-              path = deploy-rs.lib.aarch64-linux.activate.nixos
-                self.nixosConfigurations.rpi4-01;
-              remoteBuild = false;
-            };
+        nodes =
+          let
+            mkNode = (
+              hostname: {
+                inherit hostname;
+                profiles.system.path =
+                  deploy-rs.lib.aarch64-linux.activate.nixos
+                    self.nixosConfigurations."${hostname}";
+              }
+            );
+          in
+          {
+            rpi4-01 = mkNode "rpi4-01";
+            rpi4-02 = mkNode "rpi4-02";
+            rpi4-03 = mkNode "rpi4-03";
+            rpi4-04 = mkNode "rpi4-04";
           };
-          rpi4-02 = {
-            hostname = "rpi4-02";
-            profiles.system = {
-              path = deploy-rs.lib.aarch64-linux.activate.nixos
-                self.nixosConfigurations.rpi4-02;
-              remoteBuild = false;
-            };
-          };
-          rpi4-03 = {
-            hostname = "rpi4-03";
-            profiles.system = {
-              path = deploy-rs.lib.aarch64-linux.activate.nixos
-                self.nixosConfigurations.rpi4-03;
-              remoteBuild = false;
-            };
-          };
-          rpi4-04 = {
-            hostname = "rpi4-04";
-            profiles.system = {
-              path = deploy-rs.lib.aarch64-linux.activate.nixos
-                self.nixosConfigurations.rpi4-04;
-              remoteBuild = false;
-            };
-          };
-        };
       };
 
-      checks = builtins.mapAttrs
-        (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
-    } // flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in {
-        devShells.default =
-          pkgs.mkShell { buildInputs = [ pkgs.deploy-rs pkgs.nixfmt ]; };
-      });
+      checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
+    }
+    // flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.deploy-rs
+            pkgs.nixfmt
+          ];
+        };
+      }
+    );
 }
