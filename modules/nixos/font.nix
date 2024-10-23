39285e7f61ceb109fa23803e33547ba6aa7213fa
ymgyt <yamaguchi7073xtt@gmail.com>
2024-10-24 02:07:07 +0900

    feat: configure fonts.packages

--- modules/nixos/font.nix
+++ modules/nixos/font.nix
@@ -1,12 +1,28 @@
 # Configure nixos specific options
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   fonts = {
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+    ];
     enableDefaultPackages = true;
     # Make sure font match with alacritty font settings
     fontconfig.defaultFonts = {
-      serif = [ "Noto Serif" "Noto Color Emoji" ];
-      sansSerif = [ "Noto Sans CJK JP" "Noto Sans" "Noto Color Emoji" ];
-      monospace = [ "Noto Sans Mono" "Noto Color Emoji" ];
+      serif = [
+        "Noto Serif"
+        "Noto Color Emoji"
+      ];
+      sansSerif = [
+        "Noto Sans CJK JP"
+        "Noto Sans"
+        "Noto Color Emoji"
+      ];
+      monospace = [
+        "Noto Sans Mono"
+        "Noto Color Emoji"
+      ];
       emoji = [ "Noto Color Emoji" ];
     };
   };
