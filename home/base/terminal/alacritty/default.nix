fa918b3558815bf16eeedbec72bbbe8c52bf5422
ymgyt <yamaguchi7073xtt@gmail.com>
2025-03-21 13:57:41 +0900

    feat: enable x11 as alacritty winit backend

--- home/base/terminal/alacritty/default.nix
+++ home/base/terminal/alacritty/default.nix
@@ -1,7 +1,16 @@
-{ ... }: {
-  programs.alacritty = { enable = true; };
+{ pkgs, ... }:
+{
+  programs.alacritty = {
+    enable = true;
+    # wayland だと window.decorations = None を指定しても
+    # title bar が隠せないので、x11 を指定する
+    package = pkgs.alacritty.overrideAttrs (oldAttrs: {
+      postInstall = ''
+        ${oldAttrs.postInstall or ""}
+        wrapProgram $out/bin/alacritty --set WINIT_UNIX_BACKEND x11
+      '';
+    });
+  };
 
-  # yaml is deprecated
-  home.file.".alacritty.yml".source = ./alacritty.yml;
   home.file.".alacritty.toml".source = ./alacritty.toml;
 }
