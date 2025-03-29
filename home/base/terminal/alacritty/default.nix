e3bf217d0c1a9d7aca012b89d65ccc1fec8b09e5
ymgyt <yamaguchi7073xtt@gmail.com>
2025-03-29 15:15:39 +0900

    feat: use pkgs-unstable alacritty

--- home/base/terminal/alacritty/default.nix
+++ home/base/terminal/alacritty/default.nix
@@ -1,15 +1,16 @@
-{ pkgs, ... }:
+{ pkgs-unstable, ... }:
 {
   programs.alacritty = {
     enable = true;
     # wayland だと window.decorations = None を指定しても
     # title bar が隠せないので、x11 を指定する
-    package = pkgs.alacritty.overrideAttrs (oldAttrs: {
-      postInstall = ''
-        ${oldAttrs.postInstall or ""}
-        wrapProgram $out/bin/alacritty --set WINIT_UNIX_BACKEND x11
-      '';
-    });
+    # package = pkgs.alacritty.overrideAttrs (oldAttrs: {
+    #   postInstall = ''
+    #     ${oldAttrs.postInstall or ""}
+    #     wrapProgram $out/bin/alacritty --set WINIT_UNIX_BACKEND x11 --set
+    #   '';
+    # });
+    package = pkgs-unstable.alacritty;
   };
 
   home.file.".alacritty.toml".source = ./alacritty.toml;
