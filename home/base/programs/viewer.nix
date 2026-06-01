be1633f6b826fc63bbe3eb4351896f3d090f9461
ymgyt <yamaguchi7073xtt@gmail.com>
2026-06-01 12:36:43 +0900

    feat: add yazi

--- home/base/programs/viewer.nix
+++ home/base/programs/viewer.nix
@@ -2,6 +2,31 @@
 {
   programs.bat.enable = true;
   programs.eza.enable = true;
+  programs.yazi = {
+    enable = true;
+    enableNushellIntegration = true;
+    settings = {
+      mgr = {
+        show_hidden = true;
+        sort_by = "natural";
+        sort_dir_first = true;
+        sort_sensitive = false;
+        linemode = "size";
+        show_symlink = true;
+      };
+      preview = {
+        wrap = "yes";
+        tab_size = 2;
+      };
+      opener.edit = [
+        {
+          run = ''hx "$@"'';
+          block = true;
+          for = "unix";
+        }
+      ];
+    };
+  };
 
   home.packages = with pkgs; [
     file
