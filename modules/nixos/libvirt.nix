9cdd3c23ed209cb0f22b08f5834db3fd9c8ca1b8
ymgyt <yamaguchi7073xtt@gmail.com>
2026-07-11 09:57:20 +0900

    chore: configure default libvirt connection

--- modules/nixos/libvirt.nix
+++ modules/nixos/libvirt.nix
@@ -15,6 +15,8 @@ in
       enable = true;
       qemu.package = pkgs.qemu_kvm;
     };
+    # defaultの接続先を指定しておく
+    environment.variables.LIBVIRT_DEFAULT_URI = "qemu:///system";
     users.users.ymgyt.extraGroups = [ "libvirtd" ];
     environment.systemPackages = with pkgs; [
       libosinfo
